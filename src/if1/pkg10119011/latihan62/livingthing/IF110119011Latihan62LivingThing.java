57b6cd0f9b96b0a8f2070913b319edefdf55bd5c
Reihan Wiyanda <Reihan Wiyanda@Mancungs>
2020-11-26 19:36:13 +0700

    IF1-10119011-Latihan62-LivingThing

---
+++ src/if1/pkg10119011/latihan62/livingthing/IF110119011Latihan62LivingThing.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119011.latihan62.livingthing;
+
+/**
+ *
+ * @author 
+ *  Nama    : Reihan Wiyanda
+ *  Kelas   : IF-1
+ *  Nim     : 10119011
+ */
+public class IF110119011Latihan62LivingThing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LivingThing mancung = new Human ();
+        mancung.walk("Mancung");
+        mancung.breath("Mancung");
+        mancung.eat("Mancung");
+    }
+    
+}
